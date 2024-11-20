9542c2403d6ca3f20ade72cf2ef3e09118859958
Tong tong <t0ngx2@icloud.com>
2024-11-20 21:51:12 +0800

    changed main page nav bar styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 // app/page.tsx
-import { BodyContainer } from '@/components';
+import * as UI from '@/components';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <BodyContainer>
+    <UI.BodyContainer
+    navColor="var(--background)"
+    navDropoffColor="var(--navgradient)"
+    backgroundColor="var(--background2)"
+    logoColor='var(--logocolor)'
+    hoverShadowColor='var(--logoHover)'
+  >
       <p className="mb-4">Navigate to different pages:</p>
       <div className="space-x-4">
         <Link href="/page1" className="text-blue-500 hover:underline">Page 1</Link>
@@ -15,7 +21,7 @@ export default function Home() {
         <Link href="/electronics/lesson2" className="text-blue-500 hover:underline">eca</Link>
       </div>
 
-    </BodyContainer>
+    </UI.BodyContainer>
 
   );
 }
