a56b73ee35fbf27f241933f42832960dd6e46baa
Tong tong <t0ngx2@icloud.com>
2025-01-16 09:11:52 +0800

    added lesson5 ppt

--- src/app/eca/page.tsx
+++ src/app/eca/page.tsx
@@ -32,6 +32,8 @@ const Lesson2 = () => {
 
 <Link className="px-5 py-3 m-3 bg-white/10 hover:bg-white/30 transition-all duration-500 shadow-visionprohome backdrop-blur-lg rounded-full font-bold underline text-2xl" href="/eca/3dart/lesson4">Lesson4</Link>
 
+<Link className="px-5 py-3 m-3 bg-white/10 hover:bg-white/30 transition-all duration-500 shadow-visionprohome backdrop-blur-lg rounded-full font-bold underline text-2xl" href="/eca/3dart/lesson5">Lesson5</Link>
+
 
 
    </div>
