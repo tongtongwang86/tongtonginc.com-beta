1c90df7d7e21e786ba2a5be9e1352ecd5ee84257
Tong tong <t0ngx2@icloud.com>
2025-01-27 23:46:30 +0800

    added 3d models

---
+++ src/app/tests/3d/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+import dynamic from 'next/dynamic';
+import * as UI from '@/components';
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import { TextEffect } from '@/components/motion-ui/text-effect';
+
+export default function threed() {
+  const arButtons = [
+    { title: "Iron Holder", usdzFile: "/models/3d/ironholder.usdz" },
+    { title: "Keyboard Exploded", usdzFile: "/models/3d/KeyboardExploded.usdz" },
+    { title: "Maxwell", usdzFile: "/models/3d/maxwell.usdz" },
+    { title: "Revolute V2.2 Magnetics", usdzFile: "/models/3d/RevoluteV2.2 magnetics.usdz" },
+    { title: "Revolute V2.2", usdzFile: "/models/3d/RevoluteV2.2.usdz" },
+  ];
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      {arButtons.map((button, index) => (
+        <div 
+          key={index} 
+          className="bg-white/10 hover:bg-white/30 transition-all duration-500 shadow-visionprohome backdrop-blur-lg rounded-3xl p-4 flex flex-col items-center"
+        >
+          <h3 className="text-lg font-semibold mb-2">{button.title}</h3>
+          <UI.ARButton usdzFile={button.usdzFile} />
+        </div>
+      ))}
+    </div>
+  );
+}
